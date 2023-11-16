07726c9802cb85952e2f9eadc7f6f0be20e8e087
Manuel Aranda <manuelarandacavieres@gmail.com>
2023-11-15 21:25:23 -0300

    Genero Servicio con datos dummy

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,24 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
 
+    constructor(
+        private readonly productService:ProductService
+    ){}
+
     @Get()
     getAllProduct(){
-        return [
-        {
-            id:1,
-            name: 'Arroz',
-            precio: 1100,
-            URL_imagen: 'iuyiuiyiuyi',
-            stock: 10
-        },
-        {
-            id:2,
-            name: 'Tallarines',
-            precio: 1300,
-            URL_imagen: '878yiuyiy',
-            stock: 30
-        }]
+        return this.productService.getAllProduct();
     }
 }
